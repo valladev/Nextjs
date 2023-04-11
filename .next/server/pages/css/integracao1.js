14b1a9bf55fc77464b69db1c920eebc2e4b12c8f
João L. Barboza <105928845+valladev@users.noreply.github.com>
2023-04-11 10:19:38 -0300

    remove_archive_exercicios

---
+++ .next/server/pages/css/integracao1.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/css/integracao1";
+exports.ids = ["pages/css/integracao1"];
+exports.modules = {
+
+/***/ "./pages/css/integracao1.jsx":
+/*!***********************************!*\
+  !*** ./pages/css/integracao1.jsx ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ integracao)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction integracao() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n                className: \"vermelho\",\n                children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                    children: \"Texto #01\"\n                }, void 0, false, {\n                    fileName: \"/Users/joaobarboza/Documents/Udemy/Nextjs/pages/css/integracao1.jsx\",\n                    lineNumber: 4,\n                    columnNumber: 36\n                }, this)\n            }, void 0, false, {\n                fileName: \"/Users/joaobarboza/Documents/Udemy/Nextjs/pages/css/integracao1.jsx\",\n                lineNumber: 4,\n                columnNumber: 10\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n                className: \"azul\",\n                children: \"Texto #02\"\n            }, void 0, false, {\n                fileName: \"/Users/joaobarboza/Documents/Udemy/Nextjs/pages/css/integracao1.jsx\",\n                lineNumber: 5,\n                columnNumber: 10\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n                className: \"branca\",\n                children: \"Texto #03\"\n            }, void 0, false, {\n                fileName: \"/Users/joaobarboza/Documents/Udemy/Nextjs/pages/css/integracao1.jsx\",\n                lineNumber: 6,\n                columnNumber: 10\n            }, this)\n        ]\n    }, void 0, true);\n}\n;\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9jc3MvaW50ZWdyYWNhbzEuanN4LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBZSxTQUFTQSxhQUFhO0lBQ2xDLHFCQUNHOzswQkFDRyw4REFBQ0M7Z0JBQUlDLFdBQVU7MEJBQVcsNEVBQUNDOzhCQUFHOzs7Ozs7Ozs7OzswQkFDOUIsOERBQUNGO2dCQUFJQyxXQUFVOzBCQUFPOzs7Ozs7MEJBQ3RCLDhEQUFDRDtnQkFBSUMsV0FBVTswQkFBUzs7Ozs7Ozs7QUFHakMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2V4ZXJjaWNpb3MvLi9wYWdlcy9jc3MvaW50ZWdyYWNhbzEuanN4P2M3OWUiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gaW50ZWdyYWNhbygpIHtcbiAgIHJldHVybiAoXG4gICAgICA8PlxuICAgICAgICAgPGRpdiBjbGFzc05hbWU9XCJ2ZXJtZWxob1wiPjxoMT5UZXh0byAjMDE8L2gxPjwvZGl2PlxuICAgICAgICAgPGRpdiBjbGFzc05hbWU9XCJhenVsXCI+VGV4dG8gIzAyPC9kaXY+XG4gICAgICAgICA8ZGl2IGNsYXNzTmFtZT1cImJyYW5jYVwiPlRleHRvICMwMzwvZGl2PlxuICAgICAgPC8+XG4gICApXG59O1xuIl0sIm5hbWVzIjpbImludGVncmFjYW8iLCJkaXYiLCJjbGFzc05hbWUiLCJoMSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/css/integracao1.jsx\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/css/integracao1.jsx"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,9 @@
+export default function integracao() {
+   return (
+      <>
+         <div className="vermelho"><h1>Texto #01</h1></div>
+         <div className="azul">Texto #02</div>
+         <div className="branca">Texto #03</div>
+      </>
+   )
+};
